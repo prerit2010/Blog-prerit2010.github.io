ec99526d9222fe2a77391c1e37c1a57dbe987ef4
Prerit Garg <prerit2010@gmail.com>
2016-05-08 13:15:55 +0530

    Added files

---
+++ _posts/2016-05-07-Gsoc2016-Community-Bonding-Experience.md
@@ -0,0 +1,17 @@
+---
+layout: post
+cover: 'assets/images/gsoc2016-banner.png'
+title: 'GSoC2016 - Community Bonding Experience'
+date:   2016-05-06 10:18:00
+tags: GSoC2016
+subclass: 'post tag-GSoC2016'
+categories: 'prerit2010'
+logo: 'assets/images/ghost.png'
+navigation: True
+---
+
+###	 Virtual Machines Installation
+
+* Install [Virtual box for Ubuntu 15.10](https://www.virtualbox.org/wiki/Linux_Downloads)
+* Download [Fedora Workstation 23](https://getfedora.org/en/workstation/download/)
+
---
+++ deploy_blog.py
@@ -0,0 +1,36 @@
+import os, shutil, sys
+from distutils.dir_util import copy_tree
+
+destination = '/home/prerit2010/projects/prerit2010.github.io'
+source = '/home/prerit2010/projects/my blog/_site'
+ignore = ['README.md','.git', '.gitignore']
+
+for the_file in os.listdir(destination):
+    if the_file not in ignore:
+	    file_path = os.path.join(destination, the_file)
+	    try:
+	        if os.path.isfile(file_path):
+	            os.unlink(file_path)
+	        elif os.path.isdir(file_path): shutil.rmtree(file_path)
+	    except Exception as e:
+	        print(e)
+	    
+print "\nFiles Deleted from prerit2010.github.io repository!\n"
+
+copy_tree(source, destination)
+print "files copied from _site to prerit2010.github.io"
+
+try:
+	commit_message = sys.argv[1]
+except:
+	print "No commit message written"
+else:
+	import git
+	repo = git.Repo(destination)
+	print repo.git.add(u=True)
+	print repo.git.commit( m=commit_message)
+	print "Pushing......"
+	print repo.git.push()
+	print "\nPushed to prerit2010.github.io\n"
+
+
